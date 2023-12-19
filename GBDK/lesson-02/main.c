c40d3d5da2c86b37e18bdecd0bb755e8e4b7a4d5
Ian Beddie <144270645+ian-beddie@users.noreply.github.com>
2023-12-19 19:16:53 +0000

    added lesson-02

---
+++ GBDK/lesson-02/main.c
@@ -0,0 +1,29 @@
+// Copyright (c) 2023 Ian Beddie All rights reserved
+//
+// Created by: Ian Beddie
+// Created on: Dec 2023
+// This program is the space aliens program for the GameBoy.
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+#include "SpaceAliens.c"
+
+int main() {
+    // This displays a sprite to the GameBoy screen
+    set_sprite_data(0, 16, SpaceAliens);
+
+    set_sprite_tile(0, 15);
+
+    move_sprite(0, 88, 78)
+
+    set_sprite_tile(1, 5);
+    set_sprite_tile(2, 10);
+
+    move_sprite(1, 8, 144);
+    move_sprite(2, 64, 64);
+
+    SHOW_SPRITES;
+
+    return 0;
+}
---
+++ GBDK/lesson-02/main2.c
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Ian Beddie All rights reserved
+//
+// Created by: Ian Beddie
+// Created on: Dec 2023
+// This program is the space aliens program for the GameBoy.
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+#include "MetaSprites.c"
+#include "SpaceAliens.c"
+
+int main() {
+    // This displays a sprite to the GameBoy screen
+    set_sprite_data(0, 16, SpaceAliens);
+
+    set_meta_sprite_tile(0, 1, 2, 3, 4);
+
+    move_meta_sprite(0, 76, 120);
+
+    SHOW_SPRITES;
+    DISPLAYS_ON;
+
+    return 0;
+}
